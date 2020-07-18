dbb8701bb731345b51f12da6a028f8b54cc190b4
balrajyada <68457905+balrajyada@users.noreply.github.com>
2020-07-18 13:44:09 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+$razor_api_key = "rzp_test_FmXvZxPcMXvHhm";
+?>
+
+<html>  
+<body>
+
+<form action="index.php" method="get">
+Enter Amount: <input type="number" name="price"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
